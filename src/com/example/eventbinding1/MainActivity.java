20693140de7a186de089a415dbe386353f0cb2ea
luwang66 <635819835@qq.com>
2016-10-29 21:15:03 +0800

    EventBinding1！

---
+++ src/com/example/eventbinding1/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.eventbinding1;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {	
+//	private Button myBtn;
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);	
+//		 myBtn = (Button) findViewById(R.id.mybtn);
+//			myBtn.setOnClickListener(new OnClickListener() {
+//				public void onClick(View v) {
+//					Toast.makeText(MainActivity.this, "监听器中的处理方法",
+//							Toast.LENGTH_SHORT).show();
+//				}
+//			});
+	}
+	public void clickEventHandler(View source) {		
+		Toast.makeText(this, "自定义事件处理方法", Toast.LENGTH_SHORT).show();
+	}
+}
